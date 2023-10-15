e1dde3ae5ee089651dfc85e8227f99fcfc5e65f1
mishasc2ml <117428747+mishasc2ml@users.noreply.github.com>
2023-10-15 13:53:48 +0300

    Time: 4 ms (5.83%), Space: 39.3 MB (77.06%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,11 +1,18 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        int result = 0;
-        for (int i = 3; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                result += i;
-            }
+
+        return checkDiversity(n);
+    }
+    
+    private int checkDiversity(int n) {
+        if (n < 3) {
+            return 0;
         }
-        return result;
+        
+        if (n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
+            return n + checkDiversity(n - 1);
+        } else {
+            return checkDiversity(n - 1);
+        }
     }
 }
