a99ff99b807f6c10e7f372479f3c4ed70ba35826
mishasc2ml <117428747+mishasc2ml@users.noreply.github.com>
2023-10-15 19:03:39 +0300

    Time: 2 ms (48.94%), Space: 48.8 MB (18.76%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -10,40 +10,20 @@
 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        if (cloned.right == null && cloned.left == null) {
+        
+        if (original == target) {
             return cloned;
         }
-        if (cloned.val == target.val) {
-            return cloned;
+        
+        if (original == null) {
+            return null;
         }
-        HashMap<Integer, List<TreeNode>> graph = new HashMap<>();
-        graph.put(cloned.val, new ArrayList<>());
-        if (cloned.left != null) {
-            graph.get(cloned.val).add(cloned.left);
-        }
-        if (cloned.right != null) {
-            graph.get(cloned.val).add(cloned.right);
-        }
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.addAll(graph.get(cloned.val));
-        while (!queue.isEmpty()) {
-            TreeNode popped = queue.poll();
-            System.out.println(popped.val);
-            if (popped.val != target.val) {
-                graph.put(popped.val, new ArrayList<>());
-                if (popped.right != null) {
-                    graph.get(popped.val).add(popped.right); 
-                }
-                if (popped.left != null) {
-                    graph.get(popped.val).add(popped.left); 
-                }
-                if (graph.get(popped.val) != null) {
-                    queue.addAll(graph.get(popped.val));
-                }
-            } else {
-                return popped;
-            }
-        }
-        return null;
+        
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        
+        TreeNode right = getTargetCopy(original.right, cloned.right, target);
+        
+        return left != null ? left : right;
+        
     }
 }
