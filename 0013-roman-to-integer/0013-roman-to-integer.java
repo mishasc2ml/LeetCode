6e88df87ef4d1c69a9bb666fd2d30878d6ad84cc
mishasc2ml <117428747+mishasc2ml@users.noreply.github.com>
2023-11-09 22:11:12 +0300

    Time: 5 ms (52.48%), Space: 43.3 MB (85.80%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -13,8 +13,7 @@ class Solution {
     
         for (int i = 0; i< s.length(); i++) {
             if (i != s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                result += map.get(s.charAt(i+1)) - map.get(s.charAt(i));
-                i++;
+                result -= map.get(s.charAt(i));
             } else {
                 result += map.get(s.charAt(i));
             }
