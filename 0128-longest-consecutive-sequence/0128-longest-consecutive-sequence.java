f725ebf361e9c831530157e3d60c079c19e01728
mishasc2ml <117428747+mishasc2ml@users.noreply.github.com>
2024-05-02 12:33:01 +0300

    Time: 14 ms (97.91%), Space: 56.9 MB (78.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -11,18 +11,16 @@ class Solution {
         
         int curMax = 1;
         int max = 0;
-        System.out.println(Arrays.toString(nums));
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] - 1 == nums[i - 1]) {
                 curMax++;
             } else if (nums[i] == nums[i - 1]) {
                 continue;
-            } else {
-                if (curMax > max) {
-                    max = curMax;
-                    curMax = 1;
-                }
+            } else if (curMax > max) {
+                max = curMax;
                 curMax = 1;
+            } else {
+                curMax = 1; 
             }
         }
         if (curMax > max) {
