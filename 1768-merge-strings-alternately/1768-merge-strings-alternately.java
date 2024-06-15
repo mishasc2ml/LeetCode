26760510a9e0793b16a81e8a3fb9ac4587612383
mishasc2ml <117428747+mishasc2ml@users.noreply.github.com>
2024-06-15 16:28:10 +0300

    Time: 1 ms (85.71%), Space: 41.3 MB (87.23%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,18 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        int minLength = word1.length() > word2.length() ? word2.length() : word1.length();
         
-        int len = word1.length() > word2.length() ? word2.length() : word1.length();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < minLength; i++) {
+            res.append(word1.charAt(i)).append(word2.charAt(i));
         }
         
-        if (word1.length() == len) {
-            sb.append(word2.substring(len));
-        } else {
-            sb.append(word1.substring(len));
+        if (word1.length() > word2.length()) {
+            res.append(word1.substring(minLength));
+        } else{
+            res.append(word2.substring(minLength));
         }
-        return sb.toString();
+        
+        return res.toString();
     }
 }
